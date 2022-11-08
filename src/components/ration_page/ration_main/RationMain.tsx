3e72ec22454b9253f84e4c93e5420ed2e45a4ed2
kpogodev <k.pogorzelski.dev@gmail.com>
2022-11-08 19:08:02 +0000

    Initialize Ration Main Section Component

---
+++ src/components/ration_page/ration_main/RationMain.tsx
@@ -0,0 +1,6 @@
+import styles from "./RationMain.module.css"
+
+function RationMain() {
+  return <div className={styles.container}>RationMain</div>
+}
+export default RationMain
--- src/pages/ration.tsx
+++ src/pages/ration.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion"
 import { pageTransition } from "utils/framer-animations"
+import RationMain from "components/ration_page/ration_main/RationMain"
 import PageHeading from "components/reusable/page_heading/PageHeading"
 import LatestPosts from "components/reusable/latest_posts/LatestPosts"
 import GetInTouch from "components/reusable/get_in_touch/GetInTouch"
@@ -8,6 +9,7 @@ const Ration = () => {
   return (
     <motion.main {...pageTransition}>
       <PageHeading title='Ration' backgroundColor='#33A9FF' textColor='#fff' imgSrc='/porridge.png' />
+      <RationMain />
       <LatestPosts />
       <GetInTouch />
     </motion.main>
