8414aa52991ff129fbf8839c57c83fe21f9f9d9f
kpogodev <k.pogorzelski.dev@gmail.com>
2022-11-08 19:29:48 +0000

    Small refactoring of Category component

--- src/components/home_page/menu/Menu.tsx
+++ src/components/home_page/menu/Menu.tsx
@@ -1,13 +1,13 @@
-import Category from './category/Category'
-import Filter from './filter/Filter'
-import MealsList from './meals_list/MealsList'
-import styles from './Menu.module.css'
+import Category from "./category/Category"
+import Filter from "./filter/Filter"
+import MealsList from "./meals_list/MealsList"
+import styles from "./Menu.module.css"
 
 function Menu() {
   return (
     <div className={styles.container}>
       <Filter />
-      <Category />
+      <Category categories={["Breakfast", "Dinner", "Drinks", "Lunch", "Snack"]} />
       <MealsList />
     </div>
   )
--- src/components/home_page/menu/category/Category.tsx
+++ src/components/home_page/menu/category/Category.tsx
@@ -1,21 +1,23 @@
-import { useContext, useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, A11y } from 'swiper'
-import { MenuContext } from 'context/MenuContext'
-import useMediaQuery from 'hooks/useMediaQuery'
-import MealButton from 'components/reusable/meal_button/MealButton'
-import MealSwiperSlide from 'components/reusable/meal_swiper_slide/MealSwiperSlide'
-import BreakfastIcon from 'assets/menu-breakfast.svg'
-import LunchIcon from 'assets/menu-lunch.svg'
-import DinnerIcon from 'assets/menu-dinner.svg'
-import SnackIcon from 'assets/menu-snack.svg'
-import DrinksIcon from 'assets/menu-drinks.svg'
-import styles from './Category.module.css'
-import 'swiper/css'
-import 'swiper/css/navigation'
+import { useContext, useState } from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, A11y } from "swiper"
+import { MenuContext } from "context/MenuContext"
+import useMediaQuery from "hooks/useMediaQuery"
+import MealButton from "components/reusable/meal_button/MealButton"
+import MealSwiperSlide from "components/reusable/meal_swiper_slide/MealSwiperSlide"
+import BreakfastIcon from "assets/menu-breakfast.svg"
+import LunchIcon from "assets/menu-lunch.svg"
+import DinnerIcon from "assets/menu-dinner.svg"
+import SnackIcon from "assets/menu-snack.svg"
+import DrinksIcon from "assets/menu-drinks.svg"
+import styles from "./Category.module.css"
+import "swiper/css"
+import "swiper/css/navigation"
 
-function Category() {
-  const { matches: isMobile } = useMediaQuery('(max-width: 768px)')
+type Category = "Breakfast" | "Lunch" | "Dinner" | "Snack" | "Drinks"
+
+function Category({ categories }: { categories: Category[] }) {
+  const { matches: isMobile } = useMediaQuery("(max-width: 768px)")
   const ctx = useContext(MenuContext)
   const [selectedCategory, setSelectedCategory] = useState(ctx.categoryFilter)
 
@@ -42,53 +44,42 @@ function Category() {
     },
   }
 
+  const categoryIcon = (category: string) => {
+    switch (category) {
+      case "Breakfast":
+        return BreakfastIcon
+      case "Lunch":
+        return LunchIcon
+      case "Dinner":
+        return DinnerIcon
+      case "Snack":
+        return SnackIcon
+      case "Drinks":
+        return DrinksIcon
+      default:
+        return BreakfastIcon
+    }
+  }
+
   const mobileComponent = (
     <Swiper className={styles.swiper} {...swiperConfig}>
-      <SwiperSlide className='slide-with-visibility' data-meal='Breakfast'>
-        <MealSwiperSlide meal='Breakfast' selected={selectedCategory === 'Breakfast'}>
-          <BreakfastIcon />
-        </MealSwiperSlide>
-      </SwiperSlide>
-      <SwiperSlide className='slide-with-visibility' data-meal='Lunch'>
-        <MealSwiperSlide meal='Lunch' selected={selectedCategory === 'Lunch'}>
-          <LunchIcon />
-        </MealSwiperSlide>
-      </SwiperSlide>
-      <SwiperSlide className='slide-with-visibility' data-meal='Dinner'>
-        <MealSwiperSlide meal='Dinner' selected={selectedCategory === 'Dinner'}>
-          <DinnerIcon />
-        </MealSwiperSlide>
-      </SwiperSlide>
-      <SwiperSlide className='slide-with-visibility' data-meal='Snack'>
-        <MealSwiperSlide meal='Snack' selected={selectedCategory === 'Snack'}>
-          <SnackIcon />
-        </MealSwiperSlide>
-      </SwiperSlide>
-      <SwiperSlide className='slide-with-visibility' data-meal='Drinks'>
-        <MealSwiperSlide meal='Drinks' selected={selectedCategory === 'Drinks'}>
-          <DrinksIcon />
-        </MealSwiperSlide>
-      </SwiperSlide>
+      {categories.map((category) => (
+        <SwiperSlide key={category} className='slide-with-visibility' data-meal={category}>
+          <MealSwiperSlide meal={category} selected={selectedCategory === category}>
+            {categoryIcon(category)}
+          </MealSwiperSlide>
+        </SwiperSlide>
+      ))}
     </Swiper>
   )
 
   const desktopComponent = (
     <>
-      <MealButton meal='Breakfast' selected={selectedCategory === 'Breakfast'} handleClick={handleClick}>
-        <BreakfastIcon />
-      </MealButton>
-      <MealButton meal='Lunch' selected={selectedCategory === 'Lunch'} handleClick={handleClick}>
-        <LunchIcon />
-      </MealButton>
-      <MealButton meal='Dinner' selected={selectedCategory === 'Dinner'} handleClick={handleClick}>
-        <DinnerIcon />
-      </MealButton>
-      <MealButton meal='Snack' selected={selectedCategory === 'Snack'} handleClick={handleClick}>
-        <SnackIcon />
-      </MealButton>
-      <MealButton meal='Drinks' selected={selectedCategory === 'Drinks'} handleClick={handleClick}>
-        <DrinksIcon />
-      </MealButton>
+      {categories.map((category) => (
+        <MealButton key={category} meal={category} selected={selectedCategory === category} handleClick={handleClick}>
+          {categoryIcon(category)}
+        </MealButton>
+      ))}
     </>
   )
 
