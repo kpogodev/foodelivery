424b9651b4e3d3b32b43e91874643875dfe25b9b
kpogodev <k.pogorzelski.dev@gmail.com>
2022-12-08 21:45:55 +0000

    Motion Animation added to Ration Page

--- src/components/ration_page/ration_content/RationContent.tsx
+++ src/components/ration_page/ration_content/RationContent.tsx
@@ -3,14 +3,37 @@ import { RationPlanContext } from "context/RationPlanContext"
 import styles from "./RationContent.module.css"
 import KcalIcon from "assets/kcal.svg"
 import CalendarIcon from "assets/calendar.svg"
+import { motion } from "framer-motion"
+
+const elementsEntranceVariants = {
+  offscreen: {
+    opacity: 0,
+    y: 60,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+}
 
 function RationContent() {
   const { rationPlanData } = useContext(RationPlanContext)
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.header}>{rationPlanData.plan_name}</h2>
-      <div className={styles.info_bar}>
+    <motion.div
+      initial='offscreen'
+      whileInView='onscreen'
+      transition={{ staggerChildren: 0.05 }}
+      viewport={{ amount: 0.3, once: true }}
+      className={styles.container}
+    >
+      <motion.h2 className={styles.header} variants={elementsEntranceVariants}>
+        {rationPlanData.plan_name}
+      </motion.h2>
+      <motion.div className={styles.info_bar} variants={elementsEntranceVariants}>
         <span>
           <KcalIcon className={styles.icon} />
           {rationPlanData.plan_calories_range}
@@ -19,9 +42,11 @@ function RationContent() {
           <CalendarIcon className={styles.icon} />
           {rationPlanData.plan_length}
         </span>
-      </div>
-      <p className={styles.text}>{rationPlanData.plan_description}</p>
-    </div>
+      </motion.div>
+      <motion.p className={styles.text} variants={elementsEntranceVariants}>
+        {rationPlanData.plan_description}
+      </motion.p>
+    </motion.div>
   )
 }
 export default RationContent
