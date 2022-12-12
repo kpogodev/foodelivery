898d8eb9fba0c1d84b196dab344dbd07c0caa4fd
kpogodev <k.pogorzelski.dev@gmail.com>
2022-12-12 22:47:12 +0000

    Page Heading Component added to Menu page (hard coded data)

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,11 +1,13 @@
 import { motion } from "framer-motion"
 import { pageTransition } from "utils/framer-animations"
 import Meta from "components/reusable/meta/Meta"
+import PageHeading from "components/reusable/page_heading/PageHeading"
 
 const Menu = () => {
   return (
     <motion.main {...pageTransition}>
       <Meta />
+      <PageHeading title='Menu' backgroundColor='#39A137' textColor='#fff' imgSrc='/salmon.webp' />
     </motion.main>
   )
 }
